96ab4a39dd60c7bf775a86f733b855762dd2de59
MarianoGarcia24 <mariano.garciae24@gmail.com>
2024-04-24 10:00:18 -0300

    modified some views

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,26 +15,32 @@ const Weapon = ({object}) => {
   const icon_url = `https://community.akamai.steamstatic.com/economy/image/`
   console.log(object)
 
+  const stickers_class = `grid grid-cols-${object.Stickers.length}`
   return(
     <div className='Weapon'>
-      <h6> {object.NAME}</h6>
+      <h5> {object.NAME}</h5>
+      <p className='text-gray-500'> {object.WEAR} </p>
       <img  className='WpnImg' src={icon_url + object.icon_url} title={object.NAME}></img>
       <div><meter style={{'--value': 0.15}} min='0' max='1'> </meter></div>
-      <div className='grid grid-cols-5'>
-          {object.Stickers.map(sticker => 
-            <div >
-              <img src={sticker.icon_url} className='col-span-1 -w-1/4' title={sticker.sticker}></img>
-              </div>
+      <div className={stickers_class}>
+          {object.Stickers.map((sticker,index) => 
+           sticker.sticker != '' &&
+            <div>
+              <img src={sticker.icon_url} className={`col-span-${index} w-12 h-auto`} alt={sticker.sticker}></img>
+            </div>
           )
           }
-        <a href={object.LINK} target='_blank'>
-        <button className='button_external_site'>View on steam</button>
-        </a>
-        <a href={object.link} target='_blank'>
-        <button className='button_external_site'> Inspect in game</button>
-        </a>
+        </div>
+          <div className='grid grid-cols-2'>
+            <a href={object.LINK} target='_blank'>
+              <button className='button_external_site'>View on steam</button>
+            </a>
+            <a href={object.link} target='_blank'>
+              <button className='button_external_site'> Inspect in game</button>
+            </a>
+          </div>
+
       </div>
-      </div>    
   )
 }
 
@@ -50,7 +56,7 @@ function App() {
     
     <div>
       <h1 className='text-6xl text-red-500'>ANDA?</h1>
-      <div className='grid grid-cols-4 lg:grid-cols-4 gap-x-3'>
+      <div className='grid grid-cols-4 gap-x-3'>
         {
           items.map(object => {
             return (
